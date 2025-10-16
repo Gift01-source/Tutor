9fc937b2ebf23836f8f23ae02d055073b88cccda
soulswipe <bae23-gchimwaza@poly.ac.mw>
2025-10-16 18:19:16 +0000

    p

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
-from routes.tutors import tutors_bp
-from routes.sessions import sessions_bp
+from .routes.tutors import tutors_bp
+from .routes.sessions import sessions_bp
 
 app = Flask(__name__)
 CORS(app)
